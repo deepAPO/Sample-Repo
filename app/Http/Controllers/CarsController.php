2a393dde627d3f19d78dfdae5643b700c71dd01d
deepAPO <deep.apo.ong@gmail.com>
2021-07-22 17:33:31 +0800

    alpha

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Car;
 
 class CarsController extends Controller
 {
@@ -13,7 +14,15 @@ class CarsController extends Controller
      */
     public function index()
     {
-        return view('index');
+        //Select * FROM Car;
+
+        $cars = Car::all();
+
+        //dd($cars);
+
+        return view('index',[
+            'cars' => $cars
+        ]);
     }
 
     /**
