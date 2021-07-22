b267f4aa526832e6359098b9e8a85d9c30a70c59
deepAPO <deep.apo.ong@gmail.com>
2021-07-22 17:36:01 +0800

    betatest

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,5 +9,27 @@
                 Cars
             </h1>
         </div>
+
+
+    <div class="w-5/6 py-10">
+       @foreach ( $cars as $car )
+
+        <div class="m-auto">
+            <span class="uppercase text-blue-500 font-bold text-xs italic">
+                Founded: {{ $car->founded }} </span>
+
+            <h2 class="text-gray-700 text-5xl">
+                {{ $car->name }}:
+            </h2>
+
+            <p class="text-lg text-gray-700 py-6">
+                {{ $car->description }}
+            </p>
+
+            <hr class="mt-4 mb-8">
+        </div>
+
+        @endforeach
+        </div>
     </div>
 @endsection
